382782211ec6af9128b1d9b7a3e88862a65bf809
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-19 00:57:34 +0600

    Server setup with MongoDB done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,23 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4omgr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollections = client.db('toDoApp').collection('tasks')
+        console.log("mongodb connected");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello to do app')
